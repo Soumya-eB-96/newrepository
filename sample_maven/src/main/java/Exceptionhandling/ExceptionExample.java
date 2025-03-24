711ace7aa0ea49f87fd99d9c0f90e43ea449489d
asish <asish@ASISH>
2025-03-24 11:01:37 +0530

    Commit

---
+++ sample_maven/src/main/java/Exceptionhandling/ExceptionExample.java
@@ -0,0 +1,26 @@
+package Exceptionhandling;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=0;
+		
+		try {
+			int c = a/b;
+		}
+		/*catch(ArithmeticException e)
+		{
+			b=2;
+			int c = a/b;
+			System.out.println(c);
+			System.out.println(e);
+		}*/
+		finally
+		{
+			System.out.println("Exception");
+		}
+
+	}
+
+}
